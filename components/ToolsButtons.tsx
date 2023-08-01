dd68d322acf179043b9ecd514f1bda466002d7bd
Daniel Hasdiel <94010236+dhasdiel@users.noreply.github.com>
2023-08-01 23:20:24 +0000

    change link again again

--- components/ToolsButtons.tsx
+++ components/ToolsButtons.tsx
@@ -8,7 +8,7 @@ const ToolsButtons = () => {
       <Button>
         <a
           target="_blank"
-          href="https://calendar.google.com/calendar/event?action=TEMPLATE&tmeid=N2wzOTJrcGNtOG51amUycXM1bXByZW1ldG0gYzQ2MGE2OWRlMjI2MmFiNGRiOWE4N2E3ZDQyNTkxZjdhMTcxOWUyMGIwNjE0YzY3ZTRlODkwZGViY2NjNjQ3OUBn&tmsrc=c460a69de2262ab4db9a87a7d42591f7a1719e20b0614c67e4e890debccc6479%40group.calendar.google.com"
+          href="http://www.google.com/calendar/event?action=TEMPLATE&dates=20230822T153000Z%2F20230822T213000Z&text=%D7%97%D7%AA%D7%95%D7%A0%D7%94%20%D7%9B%D7%94%D7%9F%20%D7%97%D7%A1%D7%93%D7%99%D7%90%D7%9C%20%F0%9F%8E%89&location=%D7%92%D7%9F%20%D7%95%D7%90%D7%95%D7%9C%D7%9D%20%D7%90%D7%99%D7%A8%D7%95%D7%A2%D7%99%D7%9D%20%D7%9B%D7%A9%D7%A8%20%D7%91%D7%A8%D7%9E%D7%AA%20%D7%92%D7%9F%20-%20%D7%93%D7%99%D7%9E%D7%95%D7%9C%20%D7%A4%D7%9C%D7%98%D7%99%D7%A0%D7%95%D7%9D%2C%20%D7%93%D7%99%D7%9E%D7%95%D7%9C%2C%20%D7%A8%D7%97%D7%95%D7%91%20%D7%96%D7%90%D7%91%20%D7%96'%D7%91%D7%95%D7%98%D7%99%D7%A0%D7%A1%D7%A7%D7%99%201%2C%20%D7%A8%D7%9E%D7%AA%20%D7%92%D7%9F%2C%20%D7%99%D7%A9%D7%A8%D7%90%D7%9C&details="
         >
           <BsFillCalendarCheckFill />
         </a>
