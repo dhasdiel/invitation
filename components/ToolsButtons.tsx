6037357adf392456479f10117e7a2cfe029f5fda
Daniel Hasdiel <danielhasdiel@gmail.com>
2023-08-08 23:00:53 +0300

    button work

--- components/ToolsButtons.tsx
+++ components/ToolsButtons.tsx
@@ -1,31 +1,32 @@
+"use client"; // This is a client component 👈🏽
 import { Button } from "./ui/button";
-import { BsFillCalendarCheckFill } from "react-icons/bs";
 import { FaWaze } from "react-icons/fa";
 import "add-to-calendar-button";
 import { AddToCalendarButton } from "add-to-calendar-button-react";
 
 const ToolsButtons = () => {
   return (
-    <div className="flex gap-2 mb-2">
+    <div className="flex justify-center items-center gap-2 mb-2">
       <AddToCalendarButton
-        debug="true"
         name="חתונה כהן חסדיאל 🎉"
         startDate="2023-08-22"
         startTime="17:30"
         endDate="2023-08-22"
         endTime="23:30"
         timeZone="Asia/Jerusalem"
-        location="דימול פלטינום רמת גן"
+        location="גן ואולם אירועים כשר ברמת גן - דימול פלטינום, דימול, רחוב זאב ז'בוטינסקי 1, רמת גן, ישראל"
         recurrence="daily"
         recurrence_interval="1"
         options={["Apple", "Google"]}
         listStyle="modal"
         trigger="click"
-        // hideTextLabelButton
         label="שמור ליומן"
         lightMode="dark"
+        size="4"
+        hideCheckmark
+        styleDark="--btn-background: #0f172a;"
       />
-      <Button>
+      <Button className=" shadow-xl">
         <a target="_blank" href="https://waze.com/ul/hsv8wxbs6w">
           <FaWaze />
         </a>
